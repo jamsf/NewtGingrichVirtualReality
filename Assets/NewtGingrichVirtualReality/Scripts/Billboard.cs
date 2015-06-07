c2e3dc0fe5f200e64e3b29ec871c875c7dce2a46
James Farmer <JFarmer54@gmail.com>
2015-06-06 23:50:00 -0700

    Little Newts added and skybox

---
+++ Assets/NewtGingrichVirtualReality/Scripts/Billboard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+	float timer;
+	Vector3 startPos;
+	Vector3 endPos;
+
+	void Start()
+	{
+		RandomPosition();
+	}
+
+	void RandomPosition()
+	{
+		timer = Time.time;
+		startPos = transform.position;
+		endPos = new Vector3(Random.Range(-50f, 50f), Random.Range(-100f, 100f), 0);
+	}
+	void Update()
+	{
+		transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+			Camera.main.transform.rotation * Vector3.up);
+	}
+}
